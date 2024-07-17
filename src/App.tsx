53682464e6369e379cdae6976632f9eda5a552bf
Oscar <oscars-email@protonmail.com>
2024-07-17 11:51:52 +0200

    Adding changes from PC

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,71 @@
-import React from 'react';
-import './index.css'
+import React from "react";
+import "./index.css";
 
 export function App() {
   return (
     <div className="banner">
-      <div className="slider" style={{ "--quantity": "10" } as React.CSSProperties}>
-        <div className="item" style={{ '--position': "1" } as React.CSSProperties}>
+      <div
+        className="slider"
+        style={{ "--quantity": "10" } as React.CSSProperties}
+      >
+        <div
+          className="item"
+          style={{ "--position": "1" } as React.CSSProperties}
+        >
           <img src="/images/pic1.jpg" alt="" />
         </div>
-        <div className="item" style={{ '--position': "2"} as React.CSSProperties}>
+        <div
+          className="item"
+          style={{ "--position": "2" } as React.CSSProperties}
+        >
           <img src="/images/pic2.jpg" alt="" />
         </div>
-        <div className="item" style={{ '--position': "3" } as React.CSSProperties}>
+        <div
+          className="item"
+          style={{ "--position": "3" } as React.CSSProperties}
+        >
           <img src="/images/pic3.jpg" alt="" />
         </div>
-        <div className="item" style={{ '--position': "4"} as React.CSSProperties}>
+        <div
+          className="item"
+          style={{ "--position": "4" } as React.CSSProperties}
+        >
           <img src="/images/pic4.jpg" alt="" />
         </div>
-        <div className="item" style={{ '--position': "5" } as React.CSSProperties}>
+        <div
+          className="item"
+          style={{ "--position": "5" } as React.CSSProperties}
+        >
           <img src="/images/pic5.jpg" alt="" />
         </div>
-        <div className="item" style={{ '--position': "6"} as React.CSSProperties}>
+        <div
+          className="item"
+          style={{ "--position": "6" } as React.CSSProperties}
+        >
           <img src="/images/pic6.jpg" alt="" />
         </div>
-        <div className="item" style={{ '--position': "7" } as React.CSSProperties}>
+        <div
+          className="item"
+          style={{ "--position": "7" } as React.CSSProperties}
+        >
           <img src="/images/pic7.jpg" alt="" />
         </div>
-        <div className="item" style={{ '--position': "8"} as React.CSSProperties}>
+        <div
+          className="item"
+          style={{ "--position": "8" } as React.CSSProperties}
+        >
           <img src="/images/pic8.jpg" alt="" />
         </div>
-        <div className="item" style={{ '--position': "9" } as React.CSSProperties}>
+        <div
+          className="item"
+          style={{ "--position": "9" } as React.CSSProperties}
+        >
           <img src="/images/pic9.jpg" alt="" />
         </div>
-        <div className="item" style={{ '--position': "10"} as React.CSSProperties}>
+        <div
+          className="item"
+          style={{ "--position": "10" } as React.CSSProperties}
+        >
           <img src="/images/pic10.jpg" alt="" />
         </div>
       </div>
@@ -41,7 +74,7 @@ export function App() {
           <b>OSCAR RODRIGUEZ</b>
         </h1>
         <div className="author txt">
-          <h2>WEB DESIGNER (BUILT WITH REACT)</h2>
+          <h2>WEB DESIGNER (Coruña)</h2>
         </div>
         <div className="model"></div>
       </div>
