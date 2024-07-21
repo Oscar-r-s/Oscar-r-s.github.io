8ecf17815b1d59eb7043bebbf53b2604218966b2
Oscar <oscars-email@protonmail.com>
2024-07-21 12:01:11 +0200

    Model is transparent to mouse events + Deletion of text elements + Video Background

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import BackgroundVideo from "./components/BackgroundVideo";
 import Slider from "./components/Slider";
 import Content from "./components/Content";
 import "./index.css";
@@ -5,6 +6,7 @@ import "./index.css";
 export function App() {
   return (
     <div className="banner">
+      <BackgroundVideo></BackgroundVideo>
       <Slider></Slider>
       <Content></Content>
     </div>
---
+++ src/components/BackgroundVideo.tsx
@@ -0,0 +1,19 @@
+import videoBg from "/public/images/background.mp4";
+
+type Props = {};
+
+function BackgroundVideo({}: Props) {
+  return (
+    <div className="background-video-container">
+      <video
+        src={videoBg}
+        className="background-video"
+        autoPlay
+        loop
+        muted
+      ></video>
+    </div>
+  );
+}
+
+export default BackgroundVideo;
